8d3e3bd62d31a342fd4029e3383963b0502e8544
lucasreali <lucasrealioficial@gmail.com>
2025-03-27 13:58:11 -0300

    fix: Ajuste do autorization token do GitHub Oauth, agora é possivel vizualizar repositorios privados

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -5,15 +5,18 @@ namespace App\Controllers\Auth;
 class AuthController
 {
     public static function github()
-    {
-        $clientId = $_ENV['GITHUB_CLIENT_ID'];
-        $redirectUri = $_ENV['GITHUB_REDIRECT_URI'];
+{
+    $clientId = $_ENV['GITHUB_CLIENT_ID'];
+    $redirectUri = $_ENV['GITHUB_REDIRECT_URI'];
 
-        $githubAuthUrl = "https://github.com/login/oauth/authorize?client_id={$clientId}&redirect_uri={$redirectUri}&scope=user";
+    $scopes = 'user repo';
 
-        header("Location: $githubAuthUrl");
-        exit;
-    }
+    $githubAuthUrl = "https://github.com/login/oauth/authorize?client_id={$clientId}&redirect_uri={$redirectUri}&scope={$scopes}";
+
+    header("Location: $githubAuthUrl");
+    exit;
+}
+
 
     public static function logout()
     {

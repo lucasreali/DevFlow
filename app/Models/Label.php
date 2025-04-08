ee569b4a228a028c278aa7afac6ae0c852fce8f9
MateusDomingues <mdomingues18092001@gmail.com>
2025-04-08 20:42:53 -0300

    feat: criação dos metodos na classe Label , e a criação dos metodos da classe LabelController

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -18,9 +18,40 @@ class Label {
         'name' => $title,
         'color' => $color,
     ]);
+        return $db->lastInsertId();
     }
 
-    return $db->lastInsertId();
+    public static function findByUserId($userId)
+    {
+        try {
+            $db = Database::getInstance();
+            $stmt = $db->prepare('SELECT * FROM labels WHERE user_id = :user_id');
+            $stmt->execute(['user_id' => $userId]);
+            $label = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $label ?: null;
+        } 
+        catch (\PDOException $e) {
+            error_log('Database error: ' . $e->getMessage());
+            return false;
+        }
+    }
+
+    public static function updateByUserId($userId, $data)
+    {
+        $db = Database::getInstance();
+        $stmt = $db->prepare('
+            UPDATE labels
+            SET 
+                title = :title,
+                color = :color
+            WHERE user_id = :user_id'
+        );
+        return $stmt->execute([
+            'title' => $data['title'],
+            'color' => $data['color'],
+            'user_id' => $userId,
+        ]);
+    }
 
 }
 

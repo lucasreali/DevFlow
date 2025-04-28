e4d9c71a253a5017f95d967f9648eb49d0def331
MateusDomingues <mdomingues18092001@gmail.com>
2025-04-28 19:35:41 -0300

    criação label

---
+++ app/Controllers/LabelController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Label;
+
+class LabelController {
+
+    public function store() {
+
+        $userId = $_GET['user_id'];
+        $title = $_POST['title'];
+        $color = $_POST['color'];
+
+        $allowedColors = ['red', 'green', 'blue', 'yellow', 'purple'];
+        if (!in_array($color, $allowedColors)) {
+            throw new \InvalidArgumentException('Invalid color');
+        }
+
+        if (empty($title)) {
+            throw new \InvalidArgumentException('Title cannot be empty');
+        }
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $userId = $_SESSION['user']['id'];
+
+        echo $userId;
+
+        if ($userId === null) {
+            throw new \RuntimeException('User not logged in');
+        }
+
+        Label::create(userId : $userId, title : $title, color: $color);
+    } 
+
+    public function update() {
+        $userId = $_GET['user_id'];
+        $title = $_POST['title'];
+        $color = $_POST['color'];
+
+        $allowedColors = ['red', 'green', 'blue', 'yellow', 'purple'];
+        if (!in_array($color, $allowedColors)) {
+            throw new \InvalidArgumentException('Invalid color');
+        }
+
+        if (empty($title)) {
+            throw new \InvalidArgumentException('Title cannot be empty');
+        }
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $userId = $_SESSION['user']['id'];
+
+        echo $userId;
+
+        if ($userId === null) {
+            throw new \RuntimeException('User not logged in');
+        }
+
+        Label::updateByUserId(userId : $userId, title : $title, color: $color);
+    }
+
+
+    public function findByUserId() {
+        $userId = $_GET['user_id'];
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $userId = $_SESSION['user']['id'];
+
+        echo $userId;
+
+        if ($userId === null) {
+            throw new \RuntimeException('User not logged in');
+        }
+
+        Label::findByUserId(userId : $userId);
+    }
+}
+
+?>
